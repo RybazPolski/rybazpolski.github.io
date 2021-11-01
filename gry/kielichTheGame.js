8d0560e138d568a7978bc5904ee58a1ec9862d7f
Ryba <julorybarczyk@gmail.com>
2021-11-01 19:37:00 +0100

    Kielich the Game Beta
    Gotowe, ale nasyfione w CSSie :stuck_out_tongue:

--- gry/kielichTheGame.js
+++ gry/kielichTheGame.js
@@ -86,3 +86,70 @@ const empties = document.querySelectorAll('.empty');
                 break;
         }
         }
+
+
+switch(Math.floor(Math.random()*4)){
+    case 0:
+        var face = "oMarekSmyk";
+        break;
+    case 1:
+        var face = "oWojciechLaniecki";
+        break;
+    case 2:
+        var face = "oRyszardSum";
+        break;
+    case 3:
+        var face = "oMarekSzrejna";
+        break;
+}
+
+
+function win(){
+    result = document.getElementById("result")
+    document.body.style.pointerEvents = "none"
+    result.style.display = "block"
+    document.getElementById("blackbg").style.display = "block"
+    result.style.pointerEvents == "all"
+    result.innerHTML = "<img id='face' style='opacity: 100%;' src='"+face+".png'><div id='winlose' style='background-color:lightgreen;'>Udało Ci się!</div><div id='tryagain' style='background-color:green;'><a href='./kielichTheGame.html'>Spróbuj ponownie!</a>"
+}
+
+function lose(){
+    result = document.getElementById("result")
+    document.body.style.pointerEvents = "none"
+    result.style.display = "block"
+    document.getElementById("blackbg").style.display = "block"
+    result.style.pointerEvents == "all"
+    result.innerHTML = "<img id='face' style='opacity: 100%;' src='"+face+"Angry.png'><div id='winlose' style='background-color:red;'>Przegrana...</div><div id='tryagain' style='background-color:orange'><a href='./kielichTheGame.html'>Spróbuj ponownie!</a>"
+}
+
+
+function check(){
+
+    patenaCorrect == document.querySelector("#patenaCorrect")
+    palkaCorrect == document.querySelector("#palkaCorrect")
+    puryfikaterzCorrect == document.querySelector("#puryfikaterzCorrect")
+    kielichCorrect == document.querySelector("#kielichCorrect")
+    
+    if(
+        patenaCorrect.innerHTML == '\n            <div class="fillPatena" draggable="true"> </div>'
+        &&
+        palkaCorrect.innerHTML == '\n                <div class="fillPalka" draggable="true"> </div>'
+        &&
+        puryfikaterzCorrect.innerHTML == '\n                <div class="fillPuryfikaterz" draggable="true"> </div>'
+        &&
+        kielichCorrect.innerHTML == '\n                <div class="fillKielich" draggable="true"> </div>'
+
+    ){
+        win()
+    }else if(
+        patenaCorrect.innerHTML == '\n            <div class="fillPatena" draggable="true"> </div>'
+        &&
+        palkaCorrect.innerHTML == '\n                <div class="fillPalka" draggable="true"> </div>'
+        &&
+        kielichCorrect.innerHTML == '\n                <div class="fillKielich" draggable="true"> </div><div class="fillPuryfikaterz" draggable="true"> </div>'
+    ){
+        win()
+    }else{
+        lose()
+    }
+}
