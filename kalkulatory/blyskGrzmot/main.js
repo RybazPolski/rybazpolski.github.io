d47ddfbcc4e07f9ded52d93b48dac8f992cc97e3
Ryba <julorybarczyk@gmail.com>
2022-05-20 23:21:57 +0200

    kalkulator odleglości pioruna

---
+++ kalkulatory/blyskGrzmot/main.js
@@ -0,0 +1,20 @@
+var countdown, sec, milisec 
+function blysk() {
+    clearInterval(countdown)
+    sec=0
+    milisec=0
+    countdown = setInterval(function(){
+        milisec++
+        if(milisec>=10){
+            sec++
+            milisec=0
+        }
+        document.getElementById("timer").innerHTML = sec+"."+milisec+"s"
+    },100)
+}
+function grzmot(){
+    clearInterval(countdown)
+    let time = sec + (milisec*0.1)
+    let distance = Math.round((340.3*time)/10)/100
+    document.getElementById("timer").innerHTML = `Piorun uderzył ok. ${distance}km stąd.` 
+}
